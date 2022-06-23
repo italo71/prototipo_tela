95021b11c9094820e172fa600a93f91e1bc8f526
Italo Braga <102165012+italo71@users.noreply.github.com>
2022-06-23 13:31:15 -0300

    update 1.0.1
    adição de função que identifica o tipo de placa, Mercosul, padrão antigo ou placa inválida, exibe seu tipo e caso seja inválida não permite o envio.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-var btn = document.getElementById('btn');
-var data;
-
-btn.onclick = () => {
-    var data_inic = document.getElementById('data_inic').value;
-    var data_fin = document.getElementById('data_fin').value;
-    var data_restricao = document.getElementById('data_restricao').checked;
-    var data_recuperado = document.getElementById('data_recuperado').checked;
+function clearText(id){
+    document.getElementById(id).innerText = '';
 }
-
-fetch('')
-    .then(response => {
-        return response.json()
-            .then(data => data);
-    }).then((data) => {
-
-    }).catch((err) => { });
